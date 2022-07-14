5a5294354a797b4a91f57c532ede62d833b641b9
Constantin <constantin.rohde@travelping.com>
2022-07-14 14:03:33 +0000

    added log for PFCP Association Established, PFCP Association Released, PFCP Association Suspicious, PFCP Association Lost

--- upf/upf_pfcp_server.c
+++ upf/upf_pfcp_server.c
@@ -56,6 +56,8 @@
   do { } while (0)
 #endif
 
+#define upf_pfcp_associnfo(...) vlib_log_info
+
 static void upf_pfcp_make_response (pfcp_msg_t * resp, pfcp_msg_t * req);
 static void restart_response_timer (pfcp_msg_t * msg);
 
@@ -394,6 +396,12 @@ request_t1_expired (u32 seq_no)
 	  && !pool_is_free_index (gtm->nodes, msg->node))
 	{
 	  upf_node_assoc_t *n = pool_elt_at_index (gtm->nodes, msg->node);
+
+	  upf_pfcp_associnfo
+	    ("PFCP Association Lost: Node %U , Local IP %U, Remote IP %U\n",
+	     format_node_id, &n->node_id, format_ip46_address, &n->lcl_addr,
+	     IP46_TYPE_ANY, format_ip46_address, &n->rmt_addr, IP46_TYPE_ANY);
+
 	  pfcp_release_association (n);
 	}
     }
@@ -1126,9 +1134,9 @@ void upf_pfcp_server_deferred_free_msgs_by_node (u32 node)
 void upf_server_send_heartbeat (u32 node_idx)
 {
   pfcp_server_main_t *psm = &pfcp_server_main;
-  pfcp_decoded_msg_t dmsg = {
-    .type = PFCP_HEARTBEAT_REQUEST
-  };
+  pfcp_decoded_msg_t dmsg =
+  {
+  .type = PFCP_HEARTBEAT_REQUEST};
   pfcp_heartbeat_request_t *req = &dmsg.heartbeat_request;
   upf_main_t *gtm = &upf_main;
   upf_node_assoc_t *n;
@@ -1138,6 +1146,11 @@ void upf_server_send_heartbeat (u32 node_idx)
   memset (req, 0, sizeof (*req));
   SET_BIT (req->grp.fields, HEARTBEAT_REQUEST_RECOVERY_TIME_STAMP);
   req->recovery_time_stamp = psm->start_time;
+
+  upf_pfcp_associnfo
+    ("PFCP Association Suspicious: Node {%s}, Node IP {%U}, Local IP %U, Remote IP %U\n",
+     format_node_id, &n->node_id, format_ip46_address, &n->lcl_addr,
+     IP46_TYPE_ANY, format_ip46_address, &n->rmt_addr, IP46_TYPE_ANY);
 
   upf_pfcp_server_send_node_request (n, &dmsg);
 }
@@ -1309,7 +1322,7 @@ static uword
 
 	  switch (psm->expired[i] >> 24)
 	    {
-	    case 0 ... 0x7f:
+	    case 0...0 x7f:
 	      if (last_expired == psm->expired[i])
 		continue;
 	      last_expired = psm->expired[i];
